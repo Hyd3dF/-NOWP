0ed4bb02046710d803504bebffb8a1db40f1ab7c
Hyd3dF <max825420@gmail.com>
2026-06-11 00:45:53 +0300

    Delete types directory

--- types/index.ts
+++
@@ -1,4 +0,0 @@
-export type { User, UserProfile } from './user';
-export type { Wallet } from './wallet';
-export type { Transaction, TransactionType, TransactionStatus } from './transaction';
-export type { Friend, FriendStatus } from './friend';
--- types/wallet.ts
+++
@@ -1,8 +0,0 @@
-export interface Wallet {
-  id: string;
-  userId: string;
-  currency: string;
-  balance: number;
-  isDefault: boolean;
-  createdAt: string;
-}
